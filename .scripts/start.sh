99bdf5ca7e0eeb67821a368f72463ab3a151d141
Andika Kurniantoro <deekz.it@gmail.com>
2018-07-16 19:45:41 +0800

    Add reload function

--- .scripts/start.sh
+++ .scripts/start.sh
@@ -3,6 +3,8 @@
 ELIXIR_DIR="/home/elixir/app"
 cd $ELIXIR_DIR
 
+killall -2 beam.smp
+sleep 1
 elixir --detached -S mix phx.server
 
 exit $?
--- lib/elixir_demo_web/views/page_view.ex
+++ lib/elixir_demo_web/views/page_view.ex
@@ -2,7 +2,7 @@ defmodule ElixirDemoWeb.PageView do
   use ElixirDemoWeb, :view
 
 	def render("index.json", %{}) do
-		%{hello: "Elixir"}
+		%{hello: "World"}
 	end	
 
 end
